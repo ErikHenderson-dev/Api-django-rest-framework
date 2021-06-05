05b4d814070ab89e787612f9cd17634a514c0fc6
ErikHenderson-dev <erikhenderson.dev@gmail.com>
2021-06-05 19:46:58 -0300

    api em django com rest framework

---
+++ books/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    realease_year = models.IntegerField()
+    state = models.CharField(max_length=50)
+    pages = models.IntegerField()
+    publish_company = models.CharField(max_length=255)
+    create_at = models.DateField(auto_now_add=True)
